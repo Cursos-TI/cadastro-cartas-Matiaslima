0e1b555dae3577c11f51f5289a22c51b3ee39b89
Matiaslima <36802543+Matiaslima@users.noreply.github.com>
2025-11-21 12:52:10 +0000

    Inclusão da primeira versão do programa solicitado no desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,64 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+  //Variaveis da primeira carta
+  char a_Estado[30], a_Codigo[30], a_Cidade[30];
+  int a_Populacao, a_PontosTuristicos;
+  float a_Area, a_PIB;
+
+  // Variaveis da segunda carta
+  char b_Estado[30], b_Codigo[30], b_Cidade[30];
+  int b_Populacao, b_PontosTuristicos;
+  float b_Area, b_PIB;
 
   // Área para entrada de dados
+  printf("Vamos cadastrar sua primeira carta \n Digite o estado:\n");
+  scanf("%s", &a_Estado);
+  
+  printf("\nDigite o codigo: \n");
+  scanf("%s", &a_Codigo);
+
+  printf("\nDigite a cidade:\n");
+  scanf("%s", &a_Cidade);
+
+  printf("\nDigite a população:\n");
+  scanf("%d", &a_Populacao);
+
+  printf("\nDigite a area:\n");
+  scanf("%f", &a_Area);
+
+  printf("\nDigite o PIB:\n");
+  scanf("%f", &a_PIB);
+
+  printf("\nDigite o numero de pontos turisticos:\n");
+  scanf("%d", &a_PontosTuristicos);
+
+  printf("\nVamos cadastrar sua segunda carta \n Digite o estado:\n");
+  scanf("%s", &b_Estado);
+  
+  printf("\nDigite o codigo: \n");
+  scanf("%s", &b_Codigo);
+
+  printf("\nDigite a cidade:\n");
+  scanf("%s", &b_Cidade);
+
+  printf("\nDigite a população:\n");
+  scanf("%d", &b_Populacao);
+
+  printf("\nDigite a area:\n");
+  scanf("%f", &b_Area);
+
+  printf("\nDigite o PIB:\n");
+  scanf("%f", &b_PIB);
+
+  printf("\nDigite o numero de pontos turisticos:\n");
+  scanf("%d", &b_PontosTuristicos);
 
   // Área para exibição dos dados da cidade
-
+  printf("\nCarta 01:\nEstado: %s \nCodigo: %s \nCidade: %s \nPopulação: %d \nArea: %.2f \nPIB: %.2f \nPontos Turisticos: %d\n"
+          , a_Estado, a_Codigo, a_Cidade, a_Populacao, a_Area, a_PIB, a_PontosTuristicos);
+  printf("Carta 02:\nEstado: %s \nCodigo: %s \nCidade: %s \nPopulação: %d \nArea: %.2f \nPIB: %.2f \nPontos Turisticos: %d\n"
+          , b_Estado, b_Codigo, b_Cidade, b_Populacao, b_Area, b_PIB, b_PontosTuristicos);
 return 0;
 } 
